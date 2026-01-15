681add20b5d95bb632b29716512e66856d20ebd1
yardasol <oyardas2@illinois.edu>
2026-01-14 18:35:14 -0600

    add homogenizable pwr pincell model

--- openmc/examples.py
+++ openmc/examples.py
@@ -92,6 +92,102 @@ def pwr_pin_cell() -> openmc.Model:
 
     return model
 
+
+def pwr_pin_cell_homogenizable() -> openmc.Model:
+    """Create a PWR pin-cell model.
+
+    This model is identical to `pwr_pin_cell()`, but
+    has a universe structure allowing it to be homogenized by
+    `model.convert_to_multigroup()`.
+
+    Returns
+    -------
+    model : openmc.Model
+        A PWR pin-cell model
+
+    """
+    model = openmc.Model()
+
+    # Define materials.
+    fuel = openmc.Material(name='UO2 (2.4%)')
+    fuel.set_density('g/cm3', 10.29769)
+    fuel.add_nuclide('U234', 4.4843e-6)
+    fuel.add_nuclide('U235', 5.5815e-4)
+    fuel.add_nuclide('U238', 2.2408e-2)
+    fuel.add_nuclide('O16', 4.5829e-2)
+
+    clad = openmc.Material(name='Zircaloy')
+    clad.set_density('g/cm3', 6.55)
+    clad.add_nuclide('Zr90', 2.1827e-2)
+    clad.add_nuclide('Zr91', 4.7600e-3)
+    clad.add_nuclide('Zr92', 7.2758e-3)
+    clad.add_nuclide('Zr94', 7.3734e-3)
+    clad.add_nuclide('Zr96', 1.1879e-3)
+
+    hot_water = openmc.Material(name='Hot borated water')
+    hot_water.set_density('g/cm3', 0.740582)
+    hot_water.add_nuclide('H1', 4.9457e-2)
+    hot_water.add_nuclide('O16', 2.4672e-2)
+    hot_water.add_nuclide('B10', 8.0042e-6)
+    hot_water.add_nuclide('B11', 3.2218e-5)
+    hot_water.add_s_alpha_beta('c_H_in_H2O')
+
+    # Define the materials file.
+    model.materials = (fuel, clad, hot_water)
+
+    # Instantiate ZCylinder surfaces
+    pitch = 1.26
+    fuel_or = openmc.ZCylinder(x0=0, y0=0, r=0.39218, name='Fuel OR')
+    clad_or = openmc.ZCylinder(x0=0, y0=0, r=0.45720, name='Clad OR')
+    left = openmc.XPlane(x0=-pitch/2, name='left', boundary_type='reflective')
+    right = openmc.XPlane(x0=pitch/2, name='right', boundary_type='reflective')
+    bottom = openmc.YPlane(y0=-pitch/2, name='bottom',
+                           boundary_type='reflective')
+    top = openmc.YPlane(y0=pitch/2, name='top', boundary_type='reflective')
+
+    # Universes for homogenization
+    fuel_pin = openmc.Cell(name='Fuel', fill=fuel)
+    fuel_pin.region = -fuel_or
+    cladding = openmc.Cell(name='Cladding', fill=clad)
+    cladding.region = +fuel_or
+    water = openmc.Cell(name='Water', fill=hot_water)
+    water.region = +clad_or
+
+    fuel_univ = openmc.Universe(name="Homogenized Fuel")
+    fuel_univ.add_cells((fuel_pin, cladding))
+    water_univ = openmc.Universe(name="Homogenized Water")
+    water_univ.add_cell(water_univ)
+
+    # Instantiate Cells
+    fuel_pin = openmc.Cell(name='Fuel', fill=fuel_univ)
+    water = openmc.Cell(name='Water', fill=water_univ)
+
+    # Use surface half-spaces to define regions
+    fuel_pin.region = -clad_or
+    water.region = +left & -right & +bottom & -top
+
+    # Create root universe
+    model.geometry.root_universe = openmc.Universe(0, name='root universe')
+    model.geometry.root_universe.add_cells([fuel_pin, water])
+
+    model.settings.batches = 10
+    model.settings.inactive = 5
+    model.settings.particles = 100
+    model.settings.source = openmc.IndependentSource(
+        space=openmc.stats.Box([-pitch/2, -pitch/2, -1],
+                               [pitch/2, pitch/2, 1]),
+        constraints={'fissionable': True}
+    )
+
+    plot = openmc.SlicePlot.from_geometry(model.geometry)
+    plot.pixels = (300, 300)
+    plot.color_by = 'material'
+    model.plots.append(plot)
+
+    return model
+
+
+
 def pwr_2d_quarter_core() -> openmc.Model:
     """Create a 2D PWR quarter core model.
 
