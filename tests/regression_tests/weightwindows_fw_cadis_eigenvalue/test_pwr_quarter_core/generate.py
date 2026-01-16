b95c8741f568912236d9337ce70dc8d9b9b425c6
yardasol <oyardas2@illinois.edu>
2026-01-16 10:19:48 -0600

    update generate

--- tests/regression_tests/weightwindows_fw_cadis_eigenvalue/test_pwr_quarter_core/generate.py
+++ tests/regression_tests/weightwindows_fw_cadis_eigenvalue/test_pwr_quarter_core/generate.py
@@ -14,6 +14,7 @@ if convert:
     ebounds = [1e-5, 0.05, 0.15, 0.275, 0.625, 3, 1.7e4, 8.2e5, 2.e7]
     groups = openmc.mgxs.EnergyGroups(ebounds)
 
+    model.materials.cross_sections = '/home/olek/projects/cross-section-libraries/endfb71_hdf5/cross_sections.xml'
     model.convert_to_multigroup(
         domain_type='universe', domains=[1, 2, 3, 4, 5], energy_groups=groups,
         nparticles=10000, overwrite_mgxs_library=False, mgxs_path="mgxs.h5"
@@ -30,18 +31,19 @@ if convert:
     model.settings.random_ray['distance_active'] = 550.0
 
     # Overlay a mesh
-    m = 8
+    m = 4
     n = int(m * 17 * 12.5)
     pitch = 1.26 / m
-    mesh = RegularMesh()
+    mesh = RegularMesh(mesh_id=2)
     mesh.dimension = (n, n)
     mesh.lower_left = (-pitch / 2, -pitch / 2)
     mesh.upper_right = (267.75, 267.75)
     model.settings.random_ray['source_region_meshes'] = [
         (mesh, [model.geometry.root_universe])]
+    model.settings.random_ray['source_shape'] = 'linear'
 
     ww_mesh = model.tallies[0].filters[0].mesh
-    wwg = openmc.WeightWindowGenerator(
+    wwg = openmc.WeightWindowGenerator(energy_bounds=ebounds,
         method="fw_cadis", mesh=ww_mesh, max_realizations=model.settings.batches - model.settings.inactive)
     model.settings.weight_window_generators = wwg
 if weight_windows:
