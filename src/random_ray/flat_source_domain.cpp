695989115904bdf68285b865a18f14e82fccf340
yardasol <oyardas2@illinois.edu>
2026-03-24 14:13:26 -0500

    fastforward to external source region fix

--- src/random_ray/flat_source_domain.cpp
+++ src/random_ray/flat_source_domain.cpp
@@ -2299,7 +2299,6 @@ void FlatSourceDomain::update_material_density(int i)
     auto& mat {model::materials[j]};
     if (mat->density_timeseries_.size() != 0) {
       double density_factor = mat->density_timeseries_[i] / mat->density_;
-      mat->density_ = mat->density_timeseries_[i];
       for (int g_out = 0; g_out < negroups_; g_out++) {
         if (adjoint_) {
           for (int dg = 0; dg < ndgroups_; dg++) {
@@ -2323,6 +2322,30 @@ void FlatSourceDomain::update_material_density(int i)
             density_factor;
         }
       }
+    }
+  }
+
+  // Update stored fixed source based on material density change
+  if (settings::run_mode == RunMode::FIXED_SOURCE) {
+#pragma omp parallel for
+    for (int64_t sr = 0; sr < n_source_regions(); sr++) {
+      int material = source_regions_.material(sr);
+      auto& mat {model::materials[material]};
+      if (mat->density_timeseries_.size() != 0) {
+        double density_factor = mat->density_timeseries_[i] / mat->density_;
+        for (int g = 0; g < negroups_; g++) {
+          source_regions_.external_source(sr, g) /= density_factor;
+        }
+      }
+    }
+  }
+
+  // Update material density
+#pragma omp parallel for
+  for (int j = 0; j < model::materials.size(); j++) {
+    auto& mat {model::materials[j]};
+    if (mat->density_timeseries_.size() != 0) {
+      mat->density_ = mat->density_timeseries_[i];
     }
   }
 }
