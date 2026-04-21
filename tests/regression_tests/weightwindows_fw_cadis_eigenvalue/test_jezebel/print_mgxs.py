d96cd12219a847308ac9a64d8f7d7af28de2003d
yardasol <oyardas2@illinois.edu>
2026-04-21 15:09:38 -0500

    remove cruft tests

--- tests/regression_tests/weightwindows_fw_cadis_eigenvalue/test_jezebel/print_mgxs.py
+++
@@ -1,9 +0,0 @@
-import h5py
-import numpy as np
-
-f = h5py.File('mgxs.h5', 'r')
-
-for mat_name, mat in f.items():
-    m = mat['294K']
-    for xs_name, xs in m.items():
-        print(f'{mat_name} {xs_name}: {np.array(xs)}')
